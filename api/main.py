4b07c8fea85b63bb0a4438f511169eb5d596334f
Jason <130726285+jsnli@users.noreply.github.com>
2024-05-27 00:39:31 -0400

    add search route

--- api/main.py
+++ api/main.py
@@ -1,4 +1,20 @@
 from flask import Flask
+import os
+import psycopg
+import json
+from dotenv import load_dotenv
+
+if os.path.isfile('./.env'):
+    load_dotenv()
+
+user = os.environ['POSTGRES_USER']
+host = os.environ['POSTGRES_HOST']
+password = os.environ['POSTGRES_PASSWORD']
+name = os.environ['POSTGRES_DATABASE']
+
+def get_connection():
+    conn = psycopg.connect(f"host={host} dbname={name} user={user} password={password}")
+    return conn
 
 app = Flask(__name__)
 
@@ -6,6 +22,27 @@ app = Flask(__name__)
 def index():
     return "<p>index()</p>"
 
-@app.route("/about")
-def about():
-    return "<p>about()</p>"
+@app.route("/search/<name>", methods=['GET'])
+def get_game(name):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    query = "SELECT * FROM Games WHERE Name ILIKE %s"
+    name_pattern = f"%{name}%"
+    cur.execute(query, (name_pattern,))
+
+    result = []
+    applist = cur.fetchall()
+    for app in applist:
+        result.append({
+            "id": app[0],
+            "name": app[1],
+            "last_modified": app[2]
+        })
+    
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return result
